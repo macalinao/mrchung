95a0c8e536121e3ca66e6954e27404b3424e0033
Ian Macalinao <me@ian.pw>
2016-06-07 23:11:57 -0400

    setup mongodb

--- app.js
+++ app.js
@@ -1,10 +1,23 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
+const { Schema } = mongoose;
+
+const MONGODB_URI = process.env.MONGODB_URI || 'mongodb://localhost:27017';
+mongoose.connect(MONGODB_URI);
 
 const app = express();
-
 app.use(express.static(`${__dirname}/public`));
+
+const AntibodySchema = new Schema({
+  target: String,
+  catalogNumber: String,
+  hostSpecies: String,
+  reactivity: [String],
+  compatibility: [String]
+});
+
+const Antibody = mongoose.model('antibody', AntibodySchema);
 
 const PORT = process.env.PORT || 3008;
 app.listen(PORT, () => {
