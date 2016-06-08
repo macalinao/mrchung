eaf6652059e3724a347f53fbfc360f5bc06e44af
Ian Macalinao <me@ian.pw>
2016-06-07 23:30:03 -0400

    init angular

--- public/scripts.js
+++ public/scripts.js
@@ -1,1 +1,15 @@
-angular.module('mrchung', ['ui.router']);
+var app = angular.module('mrchung', ['ui.router']);
+
+app.config(function($stateProvider, $urlRouterProvider) {
+  $stateProvider
+    .state('home', {
+      url: '/',
+      controller: 'HomeCtrl',
+      templateUrl: 'templates/home.html'
+    });
+  $urlRouterProvider.otherwise('/');
+});
+
+app.controller('HomeCtrl', function($scope) {
+});
+
