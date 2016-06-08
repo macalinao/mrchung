40ec5a3852b58634adbbf99e6ce904f986a26209
Dylan Macalinao <dylanmacalinao@gmail.com>
2016-06-08 15:00:29 -0400

    testing mongoose

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const { Schema } = mongoose;
+app.use(bodyParser.urlencoded({extended: true});
 
 const MONGODB_URI = process.env.MONGODB_URI || 'mongodb://localhost:27017';
 mongoose.connect(MONGODB_URI);
@@ -26,6 +27,12 @@ app.get('/antibodies', (req, res) => {
     res.json(antibodies.map(a => a.toJSON()));
   });
 });
+        
+
+AntibodySchema.plugin(dataTables, {
+  totalKey: 'recordsTotal',
+  dataKey: 'data'
+});
 
 app.post('/antibodies', (req, res) => {
   const antibody = new Antibody(req.body);
@@ -33,6 +40,19 @@ app.post('/antibodies', (req, res) => {
     res.json(doc.toJSON());
   });
 });
+
+        app.post('/antibodies', function (req, res) {
+            var Model = require('mongoose'),
+                datatablesQuery = require('datatables-query'),
+                params = req.body,
+                query = datatablesQuery(Model);
+            
+            query.run(params).then(function (data) {
+                res.json(data);
+            }, function (err) {
+                res.status(500).json(err);
+            });
+        };
 
 const PORT = process.env.PORT || 3008;
 app.listen(PORT, () => {
