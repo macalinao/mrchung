2a6168fe14a94f43506a571f298902eedd91471b
Ian Macalinao <me@ian.pw>
2016-06-08 09:06:26 -0400

    Fix compatibility/reactivity

--- public/scripts.js
+++ public/scripts.js
@@ -11,12 +11,28 @@ app.config(function($stateProvider, $urlRouterProvider) {
 });
 
 app.controller('HomeCtrl', function($scope, $http) {
-  $scope.data = {};
+  function resetData() {
+    $scope.data = {
+      reactivity: {},
+      compatibility: {}
+    };
+  }
+  resetData();
+
+  function objToSet(obj) {
+    return Object.keys(obj).filter(k => obj[k] === true);
+  }
+
+  function adaptNgToMongoose(data) {
+    data.compatibility = objToSet(data.compatibility);
+    data.reactivity = objToSet(data.reactivity);
+    return data;
+  }
 
   $scope.submit = function() {
-    $http.post('/antibodies', $scope.data).success(function(data) {
+    $http.post('/antibodies', adaptNgToMongoose($scope.data)).success(function(data) {
       alert('Submitted!');
-      $scope.data = {};
+      resetData();
     });
   };
 });
