50a45fd827da858771e2dbf6f0e23b91a94f0f8f
Dylan Macalinao <dylanmacalinao@gmail.com>
2016-07-06 14:24:31 -0400

    why

--- public/scripts.js
+++ public/scripts.js
@@ -104,7 +104,7 @@ app.controller('ProfileCtrl', function($scope, $stateParams, $http) {
   });
 });
 
-app.controller('ProfileCtrl').config(function($sceDelegateProvider) {
+app.controller('ProfileCtrl', []).config(function($sceDelegateProvider) {
   $sceDelegateProvider.resourceUrlWhitelist([
     // Allow same origin resource loads.
     'self',
